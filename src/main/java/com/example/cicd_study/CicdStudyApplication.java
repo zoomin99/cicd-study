2d53ad20122ecd19bb7b6c146e2d6df3653d23ce
zoomin3022 <zoomin3022@gmail.com>
2024-09-02 22:50:57 +0900

    first commit

---
+++ src/main/java/com/example/cicd_study/CicdStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicd_study/CicdStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
